065d63962bd6facc34ea5e1889429f088fe35b48
andreerobert <andreerobert10@gmail.com>
2019-05-01 15:10:35 +1000

    Third commit - importing datasets to R

--- Hello World.R
+++ Hello World.R
@@ -1,1 +1,7 @@
-print("Hello World")
+library(readr)
+
+convict_registers <- read_csv("slqbritishconvictregisters201605.csv")
+
+convict_records <- read.delim("tmpMA2c8PConvict_records.txt", header = FALSE, sep = "\t")
+
+View(convict_records)
